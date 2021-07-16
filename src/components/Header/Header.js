466f98a217e66dd3546ed5c47453953d1e878022
ppfurtado <furtado.pedropaulo@gmail.com>
2021-07-16 19:03:48 -0300

    style fix images and logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,27 +1,38 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
+import logo from "../../assets/logoTitle.png";
 
-import logo from '../../assets/logoTitle.png'
-
+const ContainerHeader = styled.header`
+  display: flex;
+  width: 90%;
+  margin: 0 auto;
+`;
 
 const ContainerImageLogo = styled.div`
   padding-top: 10px;
-  display: flex;
   justify-content: center;
   background: white;
-`
+  width: 200px;
+`;
 const ImageLogo = styled.img`
   display: block;
   max-width: 100%;
-`
+  height: auto;
+`;
 
 const Header = () => {
   return (
-    <ContainerImageLogo>
-      <ImageLogo src={logo} alt="blog Apiki"/>
-    </ContainerImageLogo>
-  )
-}
+    <ContainerHeader>
+      <ContainerImageLogo>
+        <Link to="/">
+          <ImageLogo src={logo} alt="blog Apiki" />
+        </Link>
+      </ContainerImageLogo>
+      <nav></nav>
+    </ContainerHeader>
+  );
+};
 
-export default Header
+export default Header;
--- src/components/Internar/Internal.js
+++ src/components/Internar/Internal.js
@@ -1,49 +1,52 @@
-import React from 'react'
-import { useParams } from 'react-router'
-import styled from 'styled-components'
-import { GETPOST_GET } from '../../api'
-import Loading from '../../Helper/Loading'
-import useFetch from '../../Hooks/useFetch'
+import React from "react";
+import { useParams } from "react-router";
+import styled from "styled-components";
+import { GETPOST_GET } from "../../api";
+import Loading from "../../Helper/Loading";
+import useFetch from "../../Hooks/useFetch";
 
 const Wrapper = styled.div`
   height: 100vh;
   margin: 0 auto 0 auto;
   width: 80%;
-`
+  padding-bottom: 40px;
+`;
 const Internal = () => {
-  const { slug } = useParams()
-  const { data, loading, request } = useFetch()
-  
-  React.useEffect(()=>{
-    
-    async function fetchPost(){
-      const { url } = GETPOST_GET(slug)
-      const { response } = await request(url)
-      return response
+  const { slug } = useParams();
+  const { data, loading, request } = useFetch();
+
+  React.useEffect(() => {
+    async function fetchPost() {
+      const { url } = GETPOST_GET(slug);
+      const { response } = await request(url);
+      return response;
     }
-    fetchPost()
-  },[slug,request])
+    fetchPost();
+  }, [slug, request]);
 
-  
   const stringToHmtl = (str) => {
-    return {__html: str}
-  }
+    return { __html: str };
+  };
 
-  if(loading) return <Loading />
+  if (loading) return <Loading />;
   return (
-    <Wrapper className="internal" >
-      
-      {
-        data && <>
-          <img src={data[0]._embedded['wp:featuredmedia'][0].source_url} alt={data[0].title.rendered}/> 
+    <Wrapper className="internal">
+      {data && (
+        <>
+          <img
+            src={data[0]._embedded["wp:featuredmedia"][0].source_url}
+            alt={data[0].title.rendered}
+          />
           <h2>{data[0].title.rendered}</h2>
-          <div dangerouslySetInnerHTML={stringToHmtl(data[0].content.rendered)} />
-          <p>Autor: </p>
-          </>
-      }
-      
+          <div
+            dangerouslySetInnerHTML={stringToHmtl(data[0].content.rendered)}
+          />
+          <p>Autor: {data[0]._embedded.author[0].name} </p>
+          {console.log(data[0]._embedded.author[0].name)}
+        </>
+      )}
     </Wrapper>
-  )
-}
+  );
+};
 
-export default Internal
+export default Internal;
